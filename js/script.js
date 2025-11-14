58d239f2e63657368e74e092c4225f94113dbced
ilariamotta <ilaria.motta93@gmail.com>
2025-11-14 15:23:40 +0100

    feat: utilizzo API per ricevere 1 mail

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+// Bonus
+
+// Abbellire con CSS o Bootstrap
+// Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo quelle precedenti)
+// Far sì che le email vengono stampati solo quando arrivano tutti i 10 email
+
+
+
 const apiUrl = "https://flynn.boolean.careers/exercises/api/random/mail";
-console.log("ciao");
+
+
+//chiamare API del server per prelevare 1 mail random
+
+axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then(function(resp){
+    //la funzione per ricevere risposta
+    risposta = resp;
+    console.log(resp.data.response);
+});
+
+
 
